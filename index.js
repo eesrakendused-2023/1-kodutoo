b829d8d111016716da90b8df67d358f9d5d54799
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-04-10 13:55:18 +0300

    Version 2 WIP

--- index.js
+++ index.js
@@ -1,120 +1,55 @@
-class HomeClock {
-    constructor() {
-      this.use12HourFormat = false;
-      this.nightMode = false;
-      this.size = 1;
-    }
-  
-    set12HourFormat() {
-      this.use12HourFormat = !this.use12HourFormat;
-    }
-  
-    toggleNightMode() {
-      this.nightMode = !this.nightMode;
-    }
-  
-    increaseSize() {
-      if (this.size < 5) {
-        this.size += 0.25;
-      }
-    }
-  
-    decreaseSize() {
-      if (this.size > 0.25) {
-        this.size -= 0.25;
-      }
-    }
-  
-    getCurrentTime() {
-      const now = new Date();
-      let hours = now.getHours();
-      const minutes = now.getMinutes();
-      const seconds = now.getSeconds();
-  
-      let amOrPm = "";
-      if (this.use12HourFormat) {
-        amOrPm = hours >= 12 ? "pm" : "am";
-        hours %= 12;
-        hours = hours || 12;
-      }
-  
-      hours = hours < 10 ? "0" + hours : hours;
-      const minutesStr = minutes < 10 ? "0" + minutes : minutes;
-      const secondsStr = seconds < 10 ? "0" + seconds : seconds;
-  
-      const timeString = `${hours}:${minutesStr}:${secondsStr} ${amOrPm}`;
-  
-      return timeString;
-    }
-  
-    getCurrentDate() {
-      const now = new Date();
-      const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      const dayName = days[now.getDay()];
-      const dateNumber = now.getDate();
-      const monthName = months[now.getMonth()];
-      const year = now.getFullYear();
-  
-      const dateString = `${dayName}, ${dateNumber} ${monthName} ${year}`;
-  
-      return dateString;
+class Time {
+  constructor() {
+    this.nightMode = false;
+    this.format12 = true;
+    this.size = 1;
+    this.date = new Date();
+  }
+
+  toggleNightMode() {
+    this.nightMode = !this.nightMode;
+  }
+
+  toggleFormat12() {
+    this.format12 = !this.format12;
+  }
+
+  increaseSize() {
+    this.size += 0.25;
+  }
+
+  decreaseSize() {
+    if (this.size > 0.25) {
+      this.size -= 0.25;
     }
   }
-  
-  const homeClock = new HomeClock();
-  
-  function updateTime() {
-    const timeContainer = document.getElementById("timeContainer");
-    const currentTime = homeClock.getCurrentTime();
-    timeContainer.textContent = currentTime;
+
+  getTime() {
+    let hours = this.date.getHours();
+    let minutes = this.date.getMinutes();
+    let seconds = this.date.getSeconds();
+    let ampm = '';
+
+    if (this.format12) {
+      ampm = (hours >= 12) ? 'PM' : 'AM';
+      hours = (hours % 12) || 12;
+    }
+
+    hours = hours.toString().padStart(2, '0');
+    minutes = minutes.toString().padStart(2, '0');
+    seconds = seconds.toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds} ${ampm}`;
   }
-  
-  function updateDate() {
-    const dateContainer = document.getElementById("dateContainer");
-    const currentDate = homeClock.getCurrentDate();
-    dateContainer.textContent = currentDate;
+
+  getDate() {
+    const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const day = this.date.getDate();
+    const month = monthName[this.date.getMonth()];
+    const year = this.date.getFullYear();
+    const dayOfWeek = dayName[this.date.getDay()];
+
+    return `${dayOfWeek}, ${day} ${month} ${year}`;
   }
-  
-  function toggleNightMode() {
-    homeClock.toggleNightMode();
-    const body = document.getElementsByTagName("body")[0];
-    body.classList.toggle("nightMode");
-  }
-  
-  function toggleFormat() {
-    homeClock.set12HourFormat();
-    updateTime();
-  }
-  
-  function increaseSize() {
-    homeClock.increaseSize();
-    const clockContainer = document.getElementById("clockContainer");
-    clockContainer.style.fontSize = `${homeClock.size}rem`;
-  }
-  
-  function decreaseSize() {
-    homeClock.decreaseSize();
-    const clockContainer = document.getElementById("clockContainer");
-    clockContainer.style.fontSize = `${homeClock.size}rem`;
-  }
-  
-  window.onload = function () {
-    const timeContainer = document.createElement("div");
-    timeContainer.id = "timeContainer";
-    document.getElementById("clockContainer").appendChild(timeContainer);
-  
-    const dateContainer = document.createElement("div");
-    dateContainer.id = "dateContainer";
-    document.getElementById("dateContainer").appendChild(dateContainer);
-  
-    const formatButton = document.createElement("button");
-    formatButton.textContent = "Format";
-    formatButton.onclick = toggleFormat;
-    document.getElementById("buttonContainer").appendChild(formatButton);
-  
-    const nightModeButton = document.createElement("button");
-    nightModeButton.textContent = "Night Mode"
-    nightModeButton.onclick = toggleNightMode;
-    document.getElementById("buttonContainer").appendChild(nightModeButton);
-  }
+}
